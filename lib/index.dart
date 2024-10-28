ba6d456fab88b6627b49a33201470b51fc5a9547
V4nchous247 <v4nchous247@gmail.com>
2024-10-28 21:01:14 +0300

    connected firebase & handle notifications

--- lib/index.dart
+++ lib/index.dart
@@ -2,3 +2,4 @@ export './main.dart';
 export './features/index.dart';
 export './navigation/index.dart';
 export './core/index.dart';
+export './api/index.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:test_news_app/index.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseApi().initNotifications();
   setup();
   runApp(MyApp());
 }
